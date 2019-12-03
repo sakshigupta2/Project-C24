93b85e8d348507f47f7f2137dc1aa318790c10e0
sakshigupta2 <57287093+sakshigupta2@users.noreply.github.com>
2019-12-03 17:03:30 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# Project-C24
+# p5.play-boilerplate
+Boiler plate for p5.play
---
+++ sketch.js
@@ -0,0 +1,17 @@
+function setup() {
+    var canvas = createCanvas (400,400);
+}
+function draw() {
+    background("pink");
+    fill("green");
+   rect (2,380, 400,30);
+   fill("red");
+   rect (80,height-100, 100,20);
+   fill("red");
+   rect (20,height-40, 100, 20);
+   fill("red");
+   rect (20,height-60, 100,20);
+   fill("black");
+   ellipse (70,height-60,50);
+  }
+  
